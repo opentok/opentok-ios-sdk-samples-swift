3fee4aa7ff725b53cc8af8f1eef9c9a694773904
Roberto <robj.perez@gmail.com>
2019-07-03 18:00:33 +0200

    Add logging to basic video chat sample

---
+++ Basic-Video-Chat/Support/OTKLogger.h
@@ -0,0 +1,16 @@
+//
+//  OTKLogger.h
+//  Basic-Video-Chat
+//
+//  Created by rpc on 03/07/2019.
+//  Copyright © 2019 tokbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@interface OTKLogger : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
