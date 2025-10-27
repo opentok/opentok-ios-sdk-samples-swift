3f923abd8cfe9893b2975627ad64013d0a987021
aos-svg <artur.osinski@vonage.com>
2025-10-27 16:49:57 +0100

    VIDECO-13330 Basic video renderer

---
+++ Basic-Video-Renderer/Basic-Video-Renderer/ViewController.swift
@@ -0,0 +1,288 @@
+//
+//  ViewController.swift
+//  Hello-World
+//
+//  Created by Roberto Perez Cubero on 11/08/16.
+//  Copyright © 2016 tokbox. All rights reserved.
+//
+
+import UIKit
+import OpenTok
+
+// *** Fill the following variables using your own Project info  ***
+// ***            https://tokbox.com/account/#/                  ***
+// Replace with your OpenTok API key
+let kApiKey = "48073631"
+// Replace with your generated session ID
+let kSessionId = "1_MX40ODA3MzYzMX5-MTc2MTUwNTY4OTI3OX41WEVNQzJLK0hhUUZRZVdRanJCV283UXN-fn4"
+// Replace with your generated token
+let kToken = "T1==cGFydG5lcl9pZD00ODA3MzYzMSZzaWc9OWYwOGM2NzQwNmI0N2M3NWY5NzRlNjZiNTcwYjY3ZWMzODFjZGU2YzpzZXNzaW9uX2lkPTFfTVg0ME9EQTNNell6TVg1LU1UYzJNVFV3TlRZNE9USTNPWDQxV0VWTlF6SkxLMGhoVVVaUlpWZFJhbkpDVjI4M1VYTi1mbjQmY3JlYXRlX3RpbWU9MTc2MTUwNTcxOCZub25jZT0wLjQ4MDM4NzY2MTYwNTIxMjk2JnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE3NjE1MDkzMTcmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0="
+
+let kWidgetHeight = 240
+let kWidgetWidth = 320
+
+class ViewController: UIViewController {
+    lazy var session: OTSession = {
+        return OTSession(apiKey: kApiKey, sessionId: kSessionId, delegate: self)!
+    }()
+    
+    var publisher: OTPublisher?
+    var subscriber: OTSubscriber?
+    let renderer = CustomVideoRender()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        doConnect()
+    }
+    
+    /**
+     * Asynchronously begins the session connect process. Some time later, we will
+     * expect a delegate method to call us back with the results of this action.
+     */
+    fileprivate func doConnect() {
+        var error: OTError?
+        defer {
+            processError(error)
+        }
+        
+        session.connect(withToken: kToken, error: &error)
+    }
+    
+    /**
+     * Sets up an instance of OTPublisher to use with this session. OTPubilsher
+     * binds to the device camera and microphone, and will provide A/V streams
+     * to the OpenTok session.
+     */
+    fileprivate func doPublish() {
+        var error: OTError?
+        defer {
+            processError(error)
+        }
+        
+        let settings = OTPublisherSettings()
+        settings.name = UIDevice.current.name
+        publisher = OTPublisher(delegate: self, settings: settings)!
+        publisher?.videoRender = renderer
+        session.publish(publisher!, error: &error)
+        
+        if let pubView = publisher!.view {
+            pubView.frame = CGRect(x: 0, y: 0, width: kWidgetWidth, height: kWidgetHeight)
+            renderer.renderView.frame = pubView.frame
+            view.addSubview(pubView)
+            pubView.addSubview(renderer.renderView)
+        }
+    }
+    
+    /**
+     * Instantiates a subscriber for the given stream and asynchronously begins the
+     * process to begin receiving A/V content for this stream. Unlike doPublish,
+     * this method does not add the subscriber to the view hierarchy. Instead, we
+     * add the subscriber only after it has connected and begins receiving data.
+     */
+    fileprivate func doSubscribe(_ stream: OTStream) {
+        var error: OTError?
+        defer {
+            processError(error)
+        }
+        subscriber = OTSubscriber(stream: stream, delegate: self)
+        
+        session.subscribe(subscriber!, error: &error)
+    }
+    
+    fileprivate func cleanupSubscriber() {
+        subscriber?.view?.removeFromSuperview()
+        subscriber = nil
+    }
+    
+    fileprivate func cleanupPublisher() {
+        publisher!.view?.removeFromSuperview()
+        publisher = nil
+    }
+    
+    fileprivate func processError(_ error: OTError?) {
+        if let err = error {
+            DispatchQueue.main.async {
+                let controller = UIAlertController(title: "Error", message: err.localizedDescription, preferredStyle: .alert)
+                controller.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(controller, animated: true, completion: nil)
+            }
+        }
+    }
+}
+
+// MARK: - OTSession delegate callbacks
+extension ViewController: OTSessionDelegate {
+    func sessionDidConnect(_ session: OTSession) {
+        print("Session connected")
+        doPublish()
+    }
+    
+    func sessionDidDisconnect(_ session: OTSession) {
+        print("Session disconnected")
+        cleanupPublisher()
+        cleanupSubscriber()
+    }
+    
+    func session(_ session: OTSession, streamCreated stream: OTStream) {
+        print("Session streamCreated: \(stream.streamId)")
+        if subscriber == nil {
+            doSubscribe(stream)
+        }
+    }
+    
+    func session(_ session: OTSession, streamDestroyed stream: OTStream) {
+        print("Session streamDestroyed: \(stream.streamId)")
+        if let subStream = subscriber?.stream, subStream.streamId == stream.streamId {
+            cleanupSubscriber()
+        }
+    }
+    
+    func session(_ session: OTSession, didFailWithError error: OTError) {
+        print("session Failed to connect: \(error.localizedDescription)")
+    }
+    
+}
+
+// MARK: - OTPublisher delegate callbacks
+extension ViewController: OTPublisherDelegate {
+    func publisher(_ publisher: OTPublisherKit, streamCreated stream: OTStream) {
+        print("Publishing")
+    }
+    
+    func publisher(_ publisher: OTPublisherKit, streamDestroyed stream: OTStream) {
+        cleanupPublisher()
+        if let subStream = subscriber?.stream, subStream.streamId == stream.streamId {
+            cleanupSubscriber()
+        }
+    }
+    
+    func publisher(_ publisher: OTPublisherKit, didFailWithError error: OTError) {
+        print("Publisher failed: \(error.localizedDescription)")
+    }
+}
+
+// MARK: - OTSubscriber delegate callbacks
+extension ViewController: OTSubscriberDelegate {
+    func subscriberDidConnect(toStream subscriberKit: OTSubscriberKit) {
+        if let subsView = subscriber?.view {
+            subsView.frame = CGRect(x: 0, y: kWidgetHeight, width: kWidgetWidth, height: kWidgetHeight)
+            view.addSubview(subsView)
+        }
+    }
+    
+    func subscriber(_ subscriber: OTSubscriberKit, didFailWithError error: OTError) {
+        print("Subscriber failed: \(error.localizedDescription)")
+    }
+}
+
+class CustomVideoRender: NSObject, OTVideoRender {
+    
+    var renderView: UIView
+
+    override init() {
+        // Initialize with your custom render view
+        self.renderView = CustomRenderView(frame: .zero)
+        super.init()
+    }
+    
+    func renderVideoFrame(_ frame: OTVideoFrame) {
+        // Cast to your custom view type and pass the frame for rendering
+        (renderView as? CustomRenderView)?.renderVideoFrame(frame)
+    }
+}
+
+class CustomRenderView: UIView {
+    
+    // MARK: - Properties
+    var renderQueue: DispatchQueue
+    private var image: CGImage?
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        self.renderQueue = DispatchQueue.global(qos: .userInitiated)
+        super.init(frame: frame)
+        self.image = nil
+    }
+    
+    required init?(coder: NSCoder) {
+        self.renderQueue = DispatchQueue.global(qos: .userInitiated)
+        super.init(coder: coder)
+        self.image = nil
+    }
+    
+    // MARK: - Render Video Frame
+    func renderVideoFrame(_ frame: OTVideoFrame) {
+        let frameToRender = frame
+        
+        renderQueue.sync {
+            // Release previous image if any exists
+            if image != nil {
+                image = nil
+            }
+            guard let format = frame.format else { return }
+            let width = Int(format.imageWidth)
+            let height = Int(format.imageHeight)
+            let bufferSize = width * height * 3
+            
+            guard let rawYPlane = frameToRender.planes?.pointer(at: 0) else { return }
+            let yplane = rawYPlane.bindMemory(to: UInt8.self, capacity: width * height)
+            let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+            
+            // Fill RGB buffer with grayscale image (Y only)
+            for i in 0..<height {
+                for j in 0..<width {
+                    let pixelIndex = (i * width * 3) + (j * 3)
+                    let yValue = yplane[(i * width) + j]
+                    buffer[pixelIndex] = yValue
+                    buffer[pixelIndex + 1] = yValue
+                    buffer[pixelIndex + 2] = yValue
+                }
+            }
+            
+            // Release buffer when CGDataProvider is done
+            let releaseCallback: CGDataProviderReleaseDataCallback = { _, data, _ in
+                data.deallocate()
+            }
+            
+            guard let provider = CGDataProvider(dataInfo: nil, data: buffer, size: bufferSize, releaseData: releaseCallback) else {
+                buffer.deallocate()
+                return
+            }
+            
+            // Create CGImage
+            image = CGImage(
+                width: width,
+                height: height,
+                bitsPerComponent: 8,
+                bitsPerPixel: 24,
+                bytesPerRow: 3 * width,
+                space: CGColorSpaceCreateDeviceRGB(),
+                bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue),
+                provider: provider,
+                decode: nil,
+                shouldInterpolate: false,
+                intent: .defaultIntent
+            )
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.setNeedsDisplay()
+            }
+        }
+    }
+        
+    // MARK: - Drawing
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        var imgCopy: CGImage?
+        
+        renderQueue.sync {
+            if let currentImage = image {
+                imgCopy = currentImage.copy()
+            }
+        }
+        
+        if let img = imgCopy {
+            context.draw(img, in: self.bounds)
+        }
+    }
+}
---
+++ Basic-Video-Renderer/README.md
@@ -0,0 +1,78 @@
+Basic Video Renderer Sample App
+===============================
+
+The Basic-Video-Renderer app is a very simple application meant to get a new developer
+started using the OpenTok iOS SDK.
+
+Quick Start
+-----------
+
+To use this application:
+
+1. Follow the instructions in the [Quick Start](../README.md#quick-start)
+   section of the main README file for this repository.
+
+   Among other things, you need to set values for the `kApiKey`, `kSessionId`,
+   and `kToken` constants. See [Obtaining OpenTok
+   Credentials](../README.md#obtaining-opentok-credentials)
+   in the main README file for the repository.
+
+2. When you run the application, it connects to an OpenTok session and
+   publishes an audio-video stream from your device to the session.
+
+3. Run the app on a second client. You can do this by deploying the app to an
+   iOS device and testing it in the simulator at the same time. Or you can use
+   the browser_demo.html file to connect in a browser (see the following
+   section).
+
+   When the second client connects, it also publishes a stream to the session,
+   and both clients subscribe to (view) each other’s stream.
+
+Application Notes
+-----------------
+
+*   Follow the code from the `ViewController.viewDidLoad(_:)` method through
+    to the OpenTok callbacks to see how streams are created and handled in
+    the OpenTok iOS SDK.
+
+*   By default, all delegate methods from classes in the OpenTok iOS SDK are
+    invoked on the main queue. This means that you can directly modify the view
+    hierarchy from inside the callback, without any asynchronous callouts.
+
+*   When the main view loads, the ViewController calls the
+    `OTSession.initWithApiKey(_:, sessionId:,delegate:)` method to initialize
+    a Session object. The app then calls the
+    `OTSession.connectWithToken(_:, error:)` to connect to the session. The
+    `OTSessionDelegate.sessionDidConnect(_:)` message is sent when the app
+    connects to the OpenTok session.
+
+*   The `doPublish()` method of the app initializes a publisher and passes it
+    into the `OTSession.publish(_:,error:)` method. This publishes an
+    audio-video stream to the session.
+
+*   The `OTSessionDelegate.session(_:,streamCreated:)` message is sent when
+    a new stream is created in the session. In response, the
+    method calls `OTSubscriber(stream:,delegate:)`,
+    passing in the OTStream object. This causes the app to subscribe to the
+    stream.
+
+ To add a second publisher (which will display as a subscriber in your emulator), either run the app a second time in an iOS device or use the OpenTok Playground to connect to the session in a supported web browser (Chrome, Firefox, or Internet Explorer 10-11) by following the steps below:
+
+1. Go to [OpenTok Playground](https://tokbox.com/developer/tools/playground) (must be logged into your [Account](https://tokbox.com/account))
+2. Select the **Join existing session** tab
+3. Copy the session ID you used in your project file and paste it in the **Session ID** input field
+4. Click **Join Session**
+5. On the next screen, click **Connect**, then click **Publish Stream**
+6. You can adjust the Publisher options (not required), then click **Continue** to connect and begin publishing and subscribing
+
+
+Configuration Notes
+-------------------
+
+*   You can test in the iOS Simulator or on a supported iOS device. However, the
+    XCode iOS Simulator does not provide access to the camera. When running in
+    the iOS Simulator, an OTPublisher object uses a demo video instead of the
+    camera.
+
+[1]: https://tokbox.com/account/#/
+[2]: https://tokbox.com/developer/sdks/server/
