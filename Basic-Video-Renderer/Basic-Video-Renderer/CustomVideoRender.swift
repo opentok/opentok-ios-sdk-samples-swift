97edf8a3d915f4b044674bbf8ff1763008c25d78
aos-svg <artur.osinski@vonage.com>
2025-10-27 18:26:52 +0100

    VIDECO-11330 add custom render view

---
+++ Basic-Video-Renderer/Basic-Video-Renderer/CustomVideoRender.swift
@@ -0,0 +1,25 @@
+//
+//  CustomVideoRender.swift
+//  Basic-Video-Renderer
+//
+//  Created by Artur Osiński on 27/10/2025.
+//  Copyright © 2025 tokbox. All rights reserved.
+//
+
+import OpenTok
+
+class CustomVideoRender: NSObject, OTVideoRender {
+    
+    var renderView: UIView
+
+    override init() {
+        // Initialize with your custom render view
+        self.renderView = CustomRenderView(frame: .zero)
+        super.init()
+    }
+    
+    func renderVideoFrame(_ frame: OTVideoFrame) {
+        // Cast to your custom view type and pass the frame for rendering
+        (renderView as? CustomRenderView)?.renderVideoFrame(frame)
+    }
+}
